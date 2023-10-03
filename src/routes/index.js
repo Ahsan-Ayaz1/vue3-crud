f545e0842d919223f4827ebc2d00135159c43349
Ahsan-Ayaz1 <ahsanayaz7755@gmail.com>
2023-10-03 10:38:15 +0500

    Setup vue-3 project

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+// import Home from './views/Home.vue';
+
+const routes = [
+//   {
+//     path: '/',
+//     name: 'Home',
+//     component: Home,
+//   },
+  // Add more routes here
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
